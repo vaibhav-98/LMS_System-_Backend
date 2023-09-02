f835e42694bed7ddd911d61334fd4c87f6db68a1
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-09-02 15:38:47 +0530

    push my code

---
+++ server/app.js
@@ -0,0 +1,30 @@
+import express from'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import {config} from 'dotenv'
+import morgan from 'morgan'
+config()
+
+const app = express()
+
+app.use(express.json())  // It parses incoming requests with JSON payloads and is based on body-parser.
+
+app.use(cors({       //allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources
+    origin: [process.env.FRONTEND_URL],
+    credentials : true
+}))
+
+app.use(cookieParser()) //  parses cookies attached to the client request object.
+
+app.use(morgan('dev'))  // logger middleware for Node js , Using Morgan, you can easily log requests made to your Node. js server
+app.use('/ping' , function(req,res){
+    res.send('/Pong')
+})
+
+// routes of 3 module
+
+app.all('*', (req,res) => {
+    res.status(404).send('OPPS!! 404 page not found')
+})
+
+export default app;
