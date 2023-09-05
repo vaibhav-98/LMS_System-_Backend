7ea7f1d812779e3e129f0ef9cca584d9e116e092
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-09-05 15:52:50 +0530

    push code

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,44 @@
-const register = (req,res) => {
+import User from "../models/user.model.js";
+import AppError from "../utils/error.utils.js"
 
-}
+const register = async (req,res, next) => {
+    const {fullName , email , password} = req.body
+
+    if(! fullName || email || password) {
+        return next(new AppError("All fields are required", 400))
+    }
+
+     const userExists = await User.findOne({email})
+
+     if(userExists) {
+        return next(new AppError("Email alredy exists" , 400))
+     }
+
+     const user =  await User.create({
+        fullName,
+        email,
+        password,
+        avatar: {
+            public_id : email,
+            secure_url : "URL",
+        }
+     })
+
+     if(!user) {
+        return next(new AppError('user registration faild, Please try again', 400))
+     }
+
+     // TODO : File Uplode
+
+     await user.save()
+
+     register.status(201).json({
+        success: true,
+        message: 'User registration successfully',
+        user
+     })
+
+};
 
 const login = (req,res) => {
 
