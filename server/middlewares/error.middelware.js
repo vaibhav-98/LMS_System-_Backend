18cd9e8da1b521adafcfeebbb3f1846adab66dec
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-09-06 22:01:56 +0530

    code push

---
+++ server/middlewares/error.middelware.js
@@ -0,0 +1,14 @@
+const errorMiddilware = (err,req,res,next) =>{
+    err.statusCode =err.statusCode || 500;
+    err.message = err.message || "Something went wrong!";
+
+
+    return res.status(err.statusCode).json({
+        success: false,
+        message: err.message,
+        stack : err.stack
+    })
+}
+
+//aaaa
+export default errorMiddilware
