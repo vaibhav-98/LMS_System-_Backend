c8eb00b5d5a05a63d881ec2a7cf4629aea6bec1a
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-09-11 20:54:46 +0530

    push code

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -69,7 +69,24 @@ const login = async(req, res,next) => {
     return next(new AppError("All fields are required", 400));
   }
 
-  const user = User.findOne({email}).select('+password')
+  const user = await User.findOne({email}).select('+password')
+
+  if(!user || !user.comparePassword(password)){
+    return next(new AppError('Email or password does not match'))
+  }
+
+  const token = await user.generateJWTToken();
+  user.password = undefined;
+
+  res.cookie('token', token , cookieOptions)
+
+  res.status(200).json({
+    success: true,
+    message: 'User loggedin successfully',
+    user,
+  })
+
+
 };
 
 //=========================================== Logout handler (To be implemented)
