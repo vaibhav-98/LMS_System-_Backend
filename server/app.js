ef142266136334f107981dc6ac231a921f33c8e5
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-09-12 12:22:24 +0530

    apply comments  in app.js

--- server/app.js
+++ server/app.js
@@ -1,36 +1,48 @@
-import express from'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import {config} from 'dotenv'
-import morgan from 'morgan'
-import userRoutes from "./routes/user.routes.js"
-import errorMiddilware from './middlewares/error.middelware.js'
-config()
+//=============================== Import necessary modules============================================//
+import express from 'express'; // Import the Express framework
+import cors from 'cors'; // Middleware for handling Cross-Origin Resource Sharing (CORS)
+import cookieParser from 'cookie-parser'; // Middleware for parsing cookies
+import { config } from 'dotenv'; // Load environment variables from a .env file
+import morgan from 'morgan'; // Logger middleware for logging HTTP requests
+import userRoutes from "./routes/user.routes.js"; // Import user routes
+import errorMiddleware from './middlewares/error.middelware.js'; // Import error middleware
 
-const app = express()
+// Load environment variables from .env file
+config();
 
-app.use(express.json())  // It parses incoming requests with JSON payloads and is based on body-parser.
+// Create an instance of the Express application
+const app = express();
 
-app.use(cors({       //allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources
-    origin: [process.env.FRONTEND_URL],
-    credentials : true
-}))
+// Parse JSON payloads in incoming requests
+app.use(express.json());
 
-app.use(cookieParser()) //  parses cookies attached to the client request object.
+// Configure CORS middleware to allow requests from a specified origin
+app.use(cors({
+    origin: [process.env.FRONTEND_URL], // Allow requests from this origin
+    credentials: true // Enable credentials (cookies, HTTP authentication, etc.)
+}));
 
-app.use(morgan('dev'))  // logger middleware for Node js , Using Morgan, you can easily log requests made to your Node. js server
-app.use('/ping' , function(req,res){
-    res.send('/Pong')
-})
+// Parse cookies attached to the client request object
+app.use(cookieParser());
 
-//************************* routes of 3 module ******************************************//
+// Use Morgan logger middleware to log incoming requests (in 'dev' format)
+app.use(morgan('dev'));
 
-app.use('/api/v1/user', userRoutes)
+// Define a simple route to respond to a /ping endpoint
+app.use('/ping', function(req, res) {
+    res.send('/Pong');
+});
 
-app.all('*', (req,res) => {
-    res.status(404).send('OPPS!! 404 page not found')
-})
+// Routes for the 'user' module
+app.use('/api/v1/user', userRoutes);
 
-app.use(errorMiddilware)
+// Handle all other routes with a 404 error response
+app.all('*', (req, res) => {
+    res.status(404).send('OPPS!! 404 page not found');
+});
 
+// Use the custom error middleware to handle errors
+app.use(errorMiddleware);
+
+// Export the Express app for use in other modules
 export default app;
