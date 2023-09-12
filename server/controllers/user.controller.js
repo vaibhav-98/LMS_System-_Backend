362400e799cb4248af35e8f545ffb4afc4a9263f
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-09-12 11:40:39 +0530

    code push

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -35,7 +35,9 @@ const register = async (req, res, next) => {
   });
 
   if (!user) {
-    return next(new AppError("User registration failed. Please try again", 400));
+    return next(
+      new AppError("User registration failed. Please try again", 400)
+    );
   }
 
   // TODO: File Upload - You can add code here for handling file uploads, if needed.
@@ -60,43 +62,66 @@ const register = async (req, res, next) => {
   });
 };
 
-//==================================== Login handler (To be implemented)
-const login = async(req, res,next) => {
+//==================================== Login handler (To be implemented)====================================//
+const login = async (req, res, next) => {
   // Implement your login logic here
-  const { email , password} = req.body;
+  const { email, password } = req.body;
 
-  if(!email || !password){
+  if (!email || !password) {
     return next(new AppError("All fields are required", 400));
   }
 
-  const user = await User.findOne({email}).select('+password')
+  const user = await User.findOne({ email }).select("+password");
 
-  if(!user || !user.comparePassword(password)){
-    return next(new AppError('Email or password does not match'))
+  if (!user || !user.comparePassword(password)) {
+    return next(new AppError("Email or password does not match"));
   }
 
   const token = await user.generateJWTToken();
   user.password = undefined;
 
-  res.cookie('token', token , cookieOptions)
+  res.cookie("token", token, cookieOptions);
 
   res.status(200).json({
     success: true,
-    message: 'User loggedin successfully',
+    message: "User loggedin successfully",
     user,
-  })
-
-
+  });
 };
 
-//=========================================== Logout handler (To be implemented)
+//=========================================== Logout handler (To be implemented)======================================//
 const logout = (req, res) => {
   // Implement your logout logic here
+
+  res.cookie("token", null, {
+    secure: true,
+    maxAge: 0,
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "User logged out succesfully",
+  });
 };
 
-// Get user profile handler (To be implemented)
-const getProfile = (req, res) => {
-  // Implement your profile retrieval logic here
+//====================================================Get user profile handler (To be implemented)============================//
+
+// Implement your profile retrieval logic here
+
+const getProfile = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+
+    res.status(200).json({
+      success: true,
+      message: "User details",
+      user,
+    });
+  } catch (error) {
+    return next(new AppError("Failed to fetch profile", 500));
+  }
 };
 
 export { register, login, logout, getProfile };
