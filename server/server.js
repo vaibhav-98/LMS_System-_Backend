976c2c6e39ed0cb1035c572f70c52d1ec2d6097f
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-09-13 17:59:18 +0530

    add code in server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 // Import the 'app' Express instance and the 'connectionToDB' function
 import app from './app.js';
 import connectionToDB from './config/dbConnection.js';
+import cloudinary from "cloudinary"
 
 // Define the port where the server will listen. Use the environment variable PORT if available, or default to 5001.
 const PORT = process.env.PORT || 5001;
+
+
+// Cloudinary configuration
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+  console.log(cloudinary.v2.config
+    ());
+
+
 
 // Start the Express server
 app.listen(PORT, async () => {
