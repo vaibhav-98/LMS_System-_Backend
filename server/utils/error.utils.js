17967296c579dc468c2677289febc82c192329e5
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-09-15 14:55:59 +0530

    push code

--- server/utils/error.utils.js
+++ server/utils/error.utils.js
@@ -1,12 +1,17 @@
+// Define a custom error class called 'AppError' that extends the built-in 'Error' class
 class AppError extends Error {
-    constructor(message,statusCode) {
+    // Constructor for the 'AppError' class that takes a 'message' and 'statusCode'
+    constructor(message, statusCode) {
+        // Call the constructor of the parent 'Error' class with the provided 'message'
         super(message);
 
+        // Set the 'statusCode' property of the error instance to the provided 'statusCode'
         this.statusCode = statusCode;
 
-        Error.captureStackTrace(this, this.contructor)
-
+        // Capture the stack trace for the error instance, which helps with debugging
+        Error.captureStackTrace(this, this.constructor);
     }
 }
 
-export default AppError
+// Export the 'AppError' class to make it available for use in other parts of the code
+export default AppError;
