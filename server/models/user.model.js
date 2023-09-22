aa332191e24fac0496b88f7c9ad1a3c8a60bf431
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-09-22 19:09:31 +0530

    push models code

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,76 +1,94 @@
-import { Schema,model } from "mongoose";
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken'
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
-const userSchema = new Schema ({
-  
-    fullName : {
-        type : 'String',
-        required :[true , 'Name is required'],
-        minLength: [5,"Name must be at least 5 Characters"],
-        maxLength : [50,"Name should be less than 90 Chracter"],
-        lowecase: true,
-        trim:true 
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: "String",
+      required: [true, "Name is required"],
+      minLength: [5, "Name must be at least 5 Characters"],
+      maxLength: [50, "Name should be less than 90 Chracter"],
+      lowecase: true,
+      trim: true,
     },
-    email : {
-        type : 'String',
-        required :[true , 'emal is required'],
-        lowecase: true,
-        trim:true ,
-        unique :true,
-        match:[ /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/] 
+    email: {
+      type: "String",
+      required: [true, "emal is required"],
+      lowecase: true,
+      trim: true,
+      unique: true,
+      match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/],
     },
     password: {
-        type : 'String',
-        required :[true , 'Password is required'],
-        minLength: [8,"Password must be at least 5 Characters"],
-        select:false
-
+      type: "String",
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be at least 5 Characters"],
+      select: false,
+    },
+    subscription: {
+      id: String,
+      status: String,
     },
     avatar: {
-        public_id: {
-            type:'String'
-        },
-        secure_url: {
-            type:'String'
-        }
+      public_id: {
+        type: "String",
+      },
+      secure_url: {
+        type: "String",
+      },
     },
     role: {
-        type: 'String',
-        enum: ['USER', 'ADMIN'],
-        default: 'USER'
-
+      type: "String",
+      enum: ["USER", "ADMIN"],
+      default: "USER",
     },
-    forgotPasswordToke: String,
-    forgotPasswordExpiry:Date,
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+  },
+  { timestamps: true }
+);
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
-}, {timestamps:true});
+userSchema.methods = {
+  generateJWTToken: async function () {
+    return await jwt.sign(
+      {
+        id: this._id,
+        email: this.email,
+        subscription: this.subscription,
+        role: this.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "10m",
+      }
+    );
+  },
+  comparePassword: async function (plainTextPassword) {
+    return await bcrypt.compare(plainTextPassword, this.password);
+  },
+  generatePasswordResetToken: async function () {
+    const resetToken = crypto.randomBytes(20).toString("hex");
 
-userSchema.pre('save',async function(next) {
-    if(!this.isModified('password')){
-        return next()
-    }
-    this.password = await bcrypt.hash(this.password,10) 
-})
+    this.forgotPasswordToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+    this.forgotPasswordExpiry = Date.now() + 15 * 60 * 1000; // 15 min from now
 
-userSchema.methods={
-    generateJWTToken: async function() {
-        return await jwt.sign(
-            {id:this._id , email: this.email , subscription:this.subscription, role: this.role},
-            process.env.JWT_SECRET,
-            {
-                expiresIn : '10m'
-            }
-        )
-    },
-    comparePassword:async function(plainTextPassword) {
-        return await bcrypt.compare(plainTextPassword, this.password)
-    }
+    return resetToken;
+  },
+};
 
-}
+const User = model("testUser", userSchema); // Define a Mongoose model named 'User' using the 'userSchema' schema
 
-const User = model('testUser',userSchema) // Define a Mongoose model named 'User' using the 'userSchema' schema
-
-
-export default User
+export default User;
+//add
